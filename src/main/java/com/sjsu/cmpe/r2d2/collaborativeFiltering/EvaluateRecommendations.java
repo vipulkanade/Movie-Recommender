dcf2d7c0f02073a83ef81c4cda88cf2689c02ce8
Vipul Kanade <vipultechjoy@gmail.com>
2016-04-15 22:04:23 -0700

    Updated Code for Matrix Evaluation

--- src/main/java/com/sjsu/cmpe/r2d2/collaborativeFiltering/EvaluateRecommendations.java
+++ src/main/java/com/sjsu/cmpe/r2d2/collaborativeFiltering/EvaluateRecommendations.java
@@ -14,6 +14,14 @@ import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.Recommender;
 import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+import org.apache.spark.api.java.*;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.mllib.recommendation.ALS;
+import org.apache.spark.mllib.recommendation.MatrixFactorizationModel;
+import org.apache.spark.mllib.recommendation.Rating;
+import org.apache.spark.SparkConf;
+
+import scala.Tuple2;
 
 /**
  * Created by ashish on 4/8/16.
@@ -26,25 +34,16 @@ public class EvaluateRecommendations {
     static double threshold = 0.1;
     static double sum =0;
     static String typeOfRecommendation = "ub";
-
-    public static GenericUserBasedRecommender getRecommender() {
-        return recommender;
-    }
+    private final static int RANK = 5;
+    private final static int NUMBER_ITERATIONS = 8;
+    private static double lambda;
 
     public static void setRecommender(GenericUserBasedRecommender newrecommender) {
         recommender = newrecommender;
     }
 
-    public static DataModel getDataModel() {
-        return dataModel;
-    }
-
     public static void setDataModel(DataModel dataModel) {
         EvaluateRecommendations.dataModel = dataModel;
-    }
-
-    public static String getTypeOfRecommendation() {
-        return typeOfRecommendation;
     }
 
     public static void setTypeOfRecommendation(String typeOfRecommendation) {
@@ -81,9 +80,71 @@ public class EvaluateRecommendations {
                 }
             }
             System.out.println(" RMSE :" + sum/500);
-        }
+        } else if(typeOfRecommendation.equalsIgnoreCase("matrix")){
+            SparkConf spark_conf = new SparkConf().setAppName("Matrix Factor Evaluation").setMaster("local[2]");
+            JavaSparkContext java_spark_cont = new JavaSparkContext(spark_conf);
+            JavaRDD<String> file_data = java_spark_cont.textFile("src/main/resources/output/output.csv");
+            JavaRDD<Rating> rating = file_data.map(
+                    new Function<String, Rating>() {
+                        public Rating call(String s) {
+                            String[] sarray = s.split(",");
+                            return new Rating(Integer.parseInt(sarray[0]), Integer.parseInt(sarray[1]),
+                                    Double.parseDouble(sarray[2]));
+                        }
+                    }
+            );
 
+            //Test the relation between RMSE and lambda
+            lambda = 0.01;
+            while (lambda < 1) {
+                System.out.println("lambda => " + lambda + ", corresponding value of RMSE => " + getRMSE(rating));
+                lambda += 0.1;
+            }
+        }
     }
+
+    public static double getRMSE(JavaRDD<Rating> ratings) {
+        JavaRDD<Rating>[] splits = ratings.randomSplit(new double[]{0.9, 0.1}, 1000);
+        JavaRDD<Rating> test = splits[1];
+
+        MatrixFactorizationModel model = ALS.train(JavaRDD.toRDD(splits[0]), RANK, NUMBER_ITERATIONS, lambda, 1, 2000);
+
+        JavaRDD<Tuple2<Object, Object>> userProducts = test.map(
+                new Function<Rating, Tuple2<Object, Object>>() {
+                    public Tuple2<Object, Object> call(Rating r) {
+                        return new Tuple2<Object, Object>(r.user(), r.product());
+                    }
+                }
+        );
+
+        JavaPairRDD<Tuple2<Integer, Integer>, Double> predictions = JavaPairRDD.fromJavaRDD(
+                model.predict(JavaRDD.toRDD(userProducts)).toJavaRDD().map(
+                        new Function<Rating, Tuple2<Tuple2<Integer, Integer>, Double>>() {
+                            public Tuple2<Tuple2<Integer, Integer>, Double> call(Rating r){
+                                return new Tuple2<Tuple2<Integer, Integer>, Double>(new Tuple2<Integer, Integer>(r.user(), r.product()), r.rating());
+                            }
+                        }
+                ));
+        JavaRDD<Tuple2<Double, Double>> ratesAndPreds =
+                JavaPairRDD.fromJavaRDD(test.map(
+                        new Function<Rating, Tuple2<Tuple2<Integer, Integer>, Double>>() {
+                            public Tuple2<Tuple2<Integer, Integer>, Double> call(Rating r){
+                                return new Tuple2<Tuple2<Integer, Integer>, Double>(new Tuple2<Integer, Integer>(r.user(), r.product()), r.rating());
+                            }
+                        }
+                )).join(predictions).values();
+        double RMSE = JavaDoubleRDD.fromRDD(ratesAndPreds.map(
+                new Function<Tuple2<Double, Double>, Object>() {
+                    public Object call(Tuple2<Double, Double> pair) {
+                        Double err = pair._1() - pair._2();
+                        return err * err;
+                    }
+                }
+        ).rdd()).mean();
+        return Math.sqrt(RMSE);
+    }
+
+
 
     class ItemRecommenderBuilder implements RecommenderBuilder {
         public Recommender buildRecommender(DataModel model) throws TasteException {
